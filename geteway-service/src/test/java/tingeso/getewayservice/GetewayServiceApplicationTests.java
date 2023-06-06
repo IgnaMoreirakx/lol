2280e50f2b4daefcb62c7bb1ec2de1e3f1190068
IgnaMoreirakx <91294944+IgnaMoreirakx@users.noreply.github.com>
2023-06-06 09:52:15 -0400

    Servicio Gateway

---
+++ geteway-service/src/test/java/tingeso/getewayservice/GetewayServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package tingeso.getewayservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GetewayServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
