b16d291060bb4d941be935f99a91817d2eef0384
IgnaMoreira <ignacio.moreira@usach.cl>
2023-06-16 04:20:13 -0400

    Arreglado prometheus y grafana

--- proveedor-service/src/main/java/tingeso/microservicios/MicroserviciosApplication.java
+++ proveedor-service/src/main/java/tingeso/microservicios/MicroserviciosApplication.java
@@ -2,8 +2,10 @@ package tingeso.microservicios;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class MicroserviciosApplication {
 
 	public static void main(String[] args) {
